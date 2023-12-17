732272a83362b148bdba08b9a63091ecd6336221
mohammad <fakure1994@gmail.com>
2023-12-16 22:45:32 -0500

    fasdfwertw

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -60,10 +60,10 @@ resource "aws_security_group" "ec2_sg" {
   }
 }
 
-# resource "aws_iam_instance_profile" "instance_profile" {
-#   name = join("", [var.name, "-", "iam-instance-profile"])
-#   role = var.iam_role_name
-# }
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = join("", [var.name, "-", "iam-instance-profile"])
+  role = var.iam_role_name
+}
 
 
 
@@ -73,7 +73,7 @@ resource "aws_instance" "web_server" {
   key_name               = var.key_pair_name
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
   user_data              = file("scripts/userdata.sh")
-  #iam_instance_profile   = aws_iam_instance_profile.instance_profile.name
+  iam_instance_profile   = aws_iam_instance_profile.instance_profile.name
   tags                   = merge(var.tags, { Name = join("", [var.name, "-", "webserver"]) }, { Environment = var.name })
 
   # best practices as per checkov scanner
